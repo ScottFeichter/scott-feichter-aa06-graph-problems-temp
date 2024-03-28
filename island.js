0095ee850dd33561491c12adf34672fd54866b2d
Scott Feichter <scottfeichter@gmail.com>
2024-03-28 16:51:28 -0700

    weird error

--- island.js
+++ island.js
@@ -2,27 +2,27 @@ function getNeighbors(row, col, graph) {
   const neighs = [];
 
   // Check top
-  if (row - 1 >= 0 && graph[row-1][col] ===1) {
+  if (row - 1 >= 0 && graph[row - 1][col] === 1) {
     neighs.push([row - 1, col]);
   }
 
   // Check bottom
-  if (row + 1 <= graph.length && graph[row+1][col] ===1) {
+  if (row + 1 < graph.length && graph[row + 1][col] === 1) {
     neighs.push([row + 1, col]);
   }
 
   // Check left
-  if (col - 1 >= 0 && graph[row][col-1] ===1) {
+  if (col - 1 >= 0 && graph[row][col - 1] === 1) {
     neighs.push([row, col - 1]);
   }
 
+
   // Check right
-  if (col + 1 <= graph[row].length && graph[row][col+1] ===1) {
+  if (col + 1 <= graph[row].length && graph[row][col + 1] === 1) {
     neighs.push([row, col + 1]);
   }
 
   // Return neighbors
-  console.log("neighs: ", neighs);
   return neighs;
 
   // Your code here
@@ -49,24 +49,36 @@ function islandSize(row, col, graph) {
     // Then get neighs
     const neighs = getNeighbors(curr[0], curr[1], graph);
 
-
     for (let neigh of neighs) {
-      console.log("neigh: ", neigh);
+
       if (!v.has(`${neigh[0]},${neigh[1]}`)) {
         // push all the UNVISITED neighbors on top of the stack
         s.push(neigh);
         // and mark them as visited
         v.add(`${neigh[0]},${neigh[1]}`);
       }
-    }
+      console.log("s: ", s);
 
+    }
   }
   // HINT: This is what your helper function `getNeighbors` is for
   // HINT: Remember, you're storing your visited nodes as strings!
 
   // return size
-
+  console.log("size: ", size);
   return size;
 }
+
+// matrix = [
+//   [1,1,1,0,0],
+//   [0,1,1,0,1],
+//   [0,1,1,0,1],
+// ]
+
+// let row = 2;
+// let col = 4;
+// let cl = 5;
+// console.log(col + 1 <= matrix[row].length);
+// islandSize(1, 1, matrix);
 
 module.exports = [getNeighbors, islandSize];
